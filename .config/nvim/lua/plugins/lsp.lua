19715281ace958f7311a9422e94409a90b0e1146
Szymon Witt <szymonwitt2707@gmail.com>
2025-11-05 16:10:53 +0200

    feat: setup lsps to go, rust, and python

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -120,30 +120,22 @@ return {
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        gopls = {},
+
+        rust_analyzer = {},
+
+        ts_ls = {},
+
+        ruff = {},
+        basedpyright = {},
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
@@ -156,7 +148,7 @@ return {
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- populates via mason-tool-installer
+        ensure_installed = { 'ts_ls', 'ruff', 'basedpyright', 'lua_ls', 'gopls', 'rust_analyzer' },
         automatic_installation = false,
         handlers = {
           function(server_name)

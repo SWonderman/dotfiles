975d455eedd00651e0f43b4b20f7021d4466127b
Szymon Witt <szymonwitt2707@gmail.com>
2026-01-04 11:56:25 +0100

    style: change theme color

--- .config/nvim/lua/plugins/color_theme.lua
+++ .config/nvim/lua/plugins/color_theme.lua
@@ -1,8 +1,18 @@
 return {
-  'EdenEast/nightfox.nvim',
+  'datsfilipe/vesper.nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'carbonfox'
+    require('vesper').setup {
+      transparent = true,
+      italics = {
+        comments = false,
+        keywords = false,
+        functions = false,
+        strings = false,
+        variables = false,
+      },
+    }
+    vim.cmd.colorscheme 'vesper'
   end,
-  opts = ...,
+  opts = {},
 }
